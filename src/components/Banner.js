9dde326b05e6a4a4e0e672d20c1e5587f7c488e0
배지 <ttsbu@naver.com>
2022-12-31 22:52:15 +0900

    이미지 배너 UI 생성

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../api/axios';
 import requests from '../api/request';
+import './Banner.css';
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
@@ -28,7 +29,34 @@ const Banner = () => {
     setMovie(movieDetail);
   };
 
-  return <div>Banner</div>;
+  // 영화 설명 잘라주기
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substring(0, n) + '...' : str;
+  };
+
+  return (
+    <header
+      className='banner'
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: 'top center',
+        backgroundSize: 'cover',
+      }}
+    >
+      <div style={{ fontSize: 10 }} className='banner__contents'>
+        <h1 className='banner__title'>
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+
+        <div className='banner__buttons'>
+          {movie?.videos?.results[0]?.key && (
+            <button className='banner__button play'>Play</button>
+          )}
+        </div>
+        <p className='banner__description'>{truncate(movie.overview, 100)}</p>
+      </div>
+    </header>
+  );
 };
 
 export default Banner;
